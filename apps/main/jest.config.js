a23504adf3fc474284227f579e1f7b617aac1f15
whoisYeshua <alien19987@gmail.com>
2025-10-19 12:08:57 +0300

    Update `msw` package to version 2.11.5 across the monorepo and add `transformIgnorePatterns` to Jest configuration to address ESM compatibility issues. Clean up unused dependencies in `package-lock.json` for improved performance.

--- apps/main/jest.config.js
+++ apps/main/jest.config.js
@@ -11,6 +11,7 @@ const packageConfig = {
 	moduleNameMapper: pathsToModuleNameMapper(tsConfig.compilerOptions.paths, {
 		prefix: path.join('<rootDir>', tsConfig.compilerOptions.baseUrl),
 	}),
+	transformIgnorePatterns: ['node_modules/(?!until-async)'], // Jest doesn't support require ESM and we have issues with msw (https://github.com/mswjs/msw/issues/2591 & https://github.com/jestjs/jest/issues/15275)
 }
 
 const config = mergeWithBase(packageConfig)
